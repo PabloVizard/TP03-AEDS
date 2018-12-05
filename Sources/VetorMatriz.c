cdab73b81cca0a92853af4d2a04c2f169b6ebe97
mudobilingue <saulo.miranda19@gmail.com>
2018-12-04 22:03:57 -0200

    defini o i e j de acordo com os cenários

--- Sources/VetorMatriz.c
+++ Sources/VetorMatriz.c
@@ -15,5 +15,29 @@ void AlocaVetor(TipoVetor *Vetor, int Tamanho){
 }
 
 void PreencheVetor(TipoVetor *Vetor, int cenario){
+  int i = 0, j = 0;
+  if(cenario%2==1){                 // Se o cenário for ímpar, preencherá 20% das matrizes
+    if(cenario==1 || cenario==3){
+      i=73;
+    }else if(cenario==5 || cenario==7){
+      i=730;
+    }else{
+      i=7300;
+    }
+  }else{                            // Se o cenário for par, preencherá 100% das matrizes
+    if(cenario==1 || cenario==3){
+      i=365;
+    }else if(cenario==5 || cenario==7){
+      i=3650;
+    }else{
+      i=36500;
+    }
+  }
+  if((cenario%4)==1 || (cenario%4)==2){   // se o resto da divisão do cenário por 4 for igual a 1 ou 2, cada matriz terá 10 voos
+    j=10;
+  }else{                                  // se o resto da divisão do cenário por 4 for igual a 3 ou 0, cada matriz terá 100 voos
+    j=100;
+  }
+    
 
 }
